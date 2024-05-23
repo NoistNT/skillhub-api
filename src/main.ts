a5c08e6975bf739ec9d1133eb175510ae31f7081
NoistNT <arielgnr23@gmail.com>
2024-05-23 10:31:33 -0300

    Add cors and check for port env variable

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  app.enableCors();
+
+  if (!PORT) throw new Error('PORT is not defined');
+
+  await app.listen(PORT);
 }
 bootstrap();
