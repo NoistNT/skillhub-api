04484542f45601bac26f668eaac788b029dec141
NoistNT <arielgnr23@gmail.com>
2024-05-23 15:11:16 -0300

    Update user schema and create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,23 @@
+import { IsBoolean, IsEmail, IsString, IsUrl, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 20)
   firstName!: string;
+
+  @IsString()
+  @Length(3, 20)
   lastName!: string;
+
+  @IsEmail()
   email!: string;
+
+  @IsUrl()
   image!: string;
+
+  @IsBoolean()
   isActive!: boolean;
+
+  @IsBoolean()
   isAdmin!: boolean;
 }
