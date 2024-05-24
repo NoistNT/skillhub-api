359325f27ad27d574fb97a75b7b3911a9a78b9fd
NoistNT <arielgnr23@gmail.com>
2024-05-24 17:23:18 -0300

    Update job entity and dto

--- src/job/dto/create-job.dto.ts
+++ src/job/dto/create-job.dto.ts
@@ -1,9 +1,39 @@
+import {
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+import { Types } from 'mongoose';
+
 export class CreateJobDto {
+  @IsString()
+  @Length(5, 50)
   title!: string;
+
+  @IsString()
+  @Length(10, 500)
   description!: string;
+
+  @IsString()
+  @Length(5, 50)
   category!: string;
+
+  @IsString()
+  @Length(5, 80)
   location!: string;
+
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
   wage!: number;
+
+  @IsUrl()
+  @IsString()
+  @IsOptional()
   image!: string;
-  user_id!: string;
+
+  @IsOptional()
+  user_id!: Types.ObjectId;
 }
--- src/job/entities/job.entity.ts
+++ src/job/entities/job.entity.ts
@@ -1,9 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+const options = { required: true, trim: true };
+
+@Schema({ timestamps: true })
 export class Job {
+  @Prop(options)
   title!: string;
+
+  @Prop(options)
   description!: string;
-  category!: string;
+
+  @Prop(options)
   location!: string;
+
+  @Prop(options)
+  category!: string;
+
+  @Prop({ required: true })
   wage!: number;
+
+  @Prop({
+    required: true,
+    trim: true,
+    default:
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+  })
   image!: string;
-  user_id!: string;
+
+  @Prop({ required: true, ref: 'User' })
+  user_id!: Types.ObjectId;
 }
+
+export const JobSchema = SchemaFactory.createForClass(Job);
